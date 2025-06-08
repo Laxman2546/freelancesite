0a784b12535f3a48ab83cb1ced5cb7a1f889e262
ELLA LAKSHMAN <laxmannani960@gmail.com>
2025-06-08 12:30:00 +0530

    registration fixed

--- frontend/src/hooks/useAuth.js
+++ frontend/src/hooks/useAuth.js
@@ -16,7 +16,6 @@ export const useAuth = () => {
 
       const response = await axios.get(
         `${process.env.REACT_APP_BACKEND_URI}/profile`,
-        {},
         {
           withCredentials: true,
         }
