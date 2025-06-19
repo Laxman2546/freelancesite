d081dd0ae4ed9003a658d18b2f095b31935f1f75
ELLA LAKSHMAN <laxmannani960@gmail.com>
2025-06-19 13:23:40 +0530

    post gig complted last stage

--- frontend/src/pages/PostGig.jsx
+++ frontend/src/pages/PostGig.jsx
@@ -1,19 +1,34 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import FreelancerNavbar from "../components/FreelancerNavbar";
 import Steps from "../components/Steps";
 import freelancerCategories from "../utils/categories";
 import { ChevronDown } from "react-ionicons";
 import { XCircleIcon, ArrowTurnDownLeftIcon } from "@heroicons/react/24/solid";
 import { XMarkIcon } from "@heroicons/react/24/outline";
+import deliveryOptions from "../utils/deliveryDates";
+import uploadImage from "../assets/images/upload.png";
 const PostGig = () => {
-  const steps = ["Overview", "Pricing", "Description", "Gallery", "Publish"];
+  const steps = ["Overview", "Pricing", "Description", "Thumbnail", "Publish"];
   const [currentStep, setCurrentStep] = useState(0);
-  const [title, setTitle] = useState("I'm really good at,");
+  const [title, setTitle] = useState("I will do ");
   const [category, setCategory] = useState("");
   const [searchTags, setsearchTags] = useState([]);
   const [selectedsearchTags, setselectedsearchTags] = useState([]);
-  const [nextDisable, setNextDisable] = useState(true);
-
+  const [nextDisable, setNextDisable] = useState(false);
+  const [priceTitle, setPriceTitle] = useState("");
+  const [standardpriceTitle, setstandardPriceTitle] = useState("");
+  const [premiumpriceTitle, setpremiumPriceTitle] = useState("");
+  const [priceFeatures, setPriceFeatures] = useState("");
+  const [standardpriceFeatures, setstandardPriceFeatures] = useState("");
+  const [premiumpriceFeatures, setpremiumPriceFeatures] = useState("");
+  const [deliveryTime, setDeliveryTime] = useState("");
+  const [standarddeliveryTime, setstandardDeliveryTime] = useState("");
+  const [premiumdeliveryTime, setpremiumDeliveryTime] = useState("");
+  const [price, setPrice] = useState("₹");
+  const [standardprice, setstandardPrice] = useState("₹");
+  const [premiumprice, setpremiumPrice] = useState("₹");
+  const [description, setDescription] = useState("");
+  const [photo, setPhoto] = useState();
   const handleTags = () => {
     const trimmed = searchTags.trim();
     if (
@@ -31,13 +46,53 @@ const PostGig = () => {
     if (
       currentStep === 0 &&
       title.length > 19 &&
-      category != "" &&
+      category !== "" &&
       selectedsearchTags.length > 0
     ) {
       setNextDisable(false);
+    } else if (
+      currentStep === 1
+      // priceTitle.trim() !== "" &&
+      // priceFeatures.trim() !== "" &&
+      // deliveryTime !== "" &&
+      // price.trim() !== ""   //optional for future things
+    ) {
+      setNextDisable(false);
+    } else if (
+      currentStep === 2 &&
+      description != "" &&
+      description.length <= 1000
+    ) {
+      setNextDisable(false);
+    } else if (currentStep === 3 && !photo) {
+      setNextDisable(false);
+    } else {
+      setNextDisable(true);
     }
   };
+  const wordCount = description.length;
 
+  const handlePriceChange = (e) => {
+    const input = e.target.value;
+
+    if (!input.startsWith("₹")) return;
+    const numericPart = input.replace(/[^\d]/g, "");
+    setPrice("₹" + numericPart);
+  };
+  const handlestandardPriceChange = (e) => {
+    const input = e.target.value;
+
+    if (!input.startsWith("₹")) return;
+    const numericPart = input.replace(/[^\d]/g, "");
+    setstandardPrice("₹" + numericPart);
+  };
+  const handlepremiumPriceChange = (e) => {
+    const input = e.target.value;
+
+    if (!input.startsWith("₹")) return;
+    const numericPart = input.replace(/[^\d]/g, "");
+    setpremiumPrice("₹" + numericPart);
+  };
   useEffect(() => {
     checkNextPage();
   }, [checkNextPage]);
@@ -45,6 +100,15 @@ const PostGig = () => {
   const handleRemoveTags = (tags) => {
     setselectedsearchTags(selectedsearchTags.filter((s) => s !== tags));
   };
+  const fileuploadRef = useRef();
+  const cacheImage = () => {
+    const uploadedFile = fileuploadRef.current.files[0];
+    if (uploadedFile) {
+      const cachedURL = URL.createObjectURL(uploadedFile);
+      setPhoto(cachedURL);
+    }
+  };
+
   return (
     <main className="w-full min-h-screen">
       <FreelancerNavbar />
@@ -73,6 +137,13 @@ const PostGig = () => {
                     value={title}
                     onChange={(e) => setTitle(e.target.value)}
                   />
+                  <span
+                    className={` text-sm ${
+                      title.length > 19 ? "hidden" : "block"
+                    }`}
+                  >
+                    Minimum 20 letters
+                  </span>
                 </div>
               </div>
               <div className="flex flex-col md:flex-row gap-5">
@@ -115,8 +186,7 @@ const PostGig = () => {
                       selectedsearchTags.length > 4 && searchTags
                         ? "border-red-500"
                         : "border-[#d7d7d7]"
-                    } rounded-xl outline-none resize-none font-semibold text-lg`}
-                    placeholder="animation,developer"
+                    } rounded-xl outline-none resize-none  text-lg`}
                     value={searchTags}
                     onChange={(e) => {
                       e.target.value.length < 13 &&
@@ -152,12 +222,12 @@ const PostGig = () => {
                   <div className="w-full grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 gap-5  mt-8">
                     {selectedsearchTags.length > 0 &&
                       selectedsearchTags.map((search, index) => (
-                        <div className="w-full  relative">
-                          <h1 className="p-4  min-w-[150px] text-center bg-lime-900 text-white rounded-2xl font-medium">
+                        <div className="w-full  relative" key={index}>
+                          <h1 className="p-4  min-w-[180px] text-center bg-lime-900 text-white rounded-2xl font-medium">
                             #{search}
                           </h1>
                           <XMarkIcon
-                            className="size-5 absolute right-1.5 top-4.5 hover:bg-[#d7d7d7] hover:text-black rounded-full cursor-pointer text-white"
+                            className="size-5 absolute right-0 top-4.5 hover:bg-[#d7d7d7] hover:text-black rounded-full cursor-pointer text-white"
                             onClick={() => handleRemoveTags(search)}
                           />
                         </div>
@@ -168,9 +238,259 @@ const PostGig = () => {
               </div>
             </div>
           )}
+          {currentStep === 1 && (
+            <div className="w-full p-4 sm:p-6 md:p-8 bg-white rounded-xl">
+              <div className="grid grid-cols-1 sm:grid-cols-3 gap-8">
+                <div className="border border-black rounded-lg p-4 gap-4 flex flex-col">
+                  <h3 className="text-lg font-semibold mb-2 text-center">
+                    Basic
+                  </h3>
+
+                  <div className="w-full">
+                    <span className="text-md font-medium ml-1">
+                      Package Title
+                    </span>
+                    <textarea
+                      className="p-3 w-full border-2 border-[#d7d7d7] rounded-xl outline-none resize-none font-semibold text-md"
+                      value={priceTitle}
+                      onChange={(e) => setPriceTitle(e.target.value)}
+                    />
+                  </div>
+
+                  <div className="w-full">
+                    <span className="text-md font-medium ml-1">
+                      Package features
+                    </span>
+                    <textarea
+                      className="p-3 w-full border-2 border-[#d7d7d7] rounded-xl outline-none resize-none font-medium text-sm"
+                      value={priceFeatures}
+                      onChange={(e) => setPriceFeatures(e.target.value)}
+                    />
+                  </div>
+
+                  <div className="w-full">
+                    <span className="text-md font-medium ml-1">
+                      Delivery Time
+                    </span>
+                    <div className="relative cursor-pointer">
+                      <ChevronDown className="size-6 absolute right-5 top-3 pointer-events-none" />
+                      <select
+                        className="px-3 py-2 w-full rounded-xl outline-none appearance-none border-2 border-[#d9d9d9] bg-white"
+                        value={deliveryTime}
+                        onChange={(e) => setDeliveryTime(e.target.value)}
+                      >
+                        <option>Delivery</option>
+                        {deliveryOptions.map((dates) => (
+                          <option key={dates.value}>{dates.label}</option>
+                        ))}
+                      </select>
+                    </div>
+                  </div>
+
+                  <div className="w-full">
+                    <span className="text-md font-medium ml-1">Price</span>
+                    <input
+                      type="text"
+                      min={1}
+                      className="p-3 w-full border-2 border-[#d7d7d7] rounded-xl outline-none resize-none font-semibold text-md"
+                      value={price}
+                      onChange={handlePriceChange}
+                    />
+                  </div>
+                </div>
+                <div className="border border-black rounded-lg p-4 gap-4 flex flex-col">
+                  <h3 className="text-lg font-semibold mb-2 text-center">
+                    Standard
+                  </h3>
+
+                  <div className="w-full">
+                    <span className="text-md font-medium ml-1">
+                      Package Title
+                    </span>
+                    <textarea
+                      className="p-3 w-full border-2 border-[#d7d7d7] rounded-xl outline-none resize-none font-semibold text-md"
+                      value={standardpriceTitle}
+                      onChange={(e) => setstandardPriceTitle(e.target.value)}
+                    />
+                  </div>
+
+                  <div className="w-full">
+                    <span className="text-md font-medium ml-1">
+                      Package features
+                    </span>
+                    <textarea
+                      className="p-3 w-full border-2 border-[#d7d7d7] rounded-xl outline-none resize-none font-medium text-sm"
+                      value={standardpriceFeatures}
+                      onChange={(e) => setstandardPriceFeatures(e.target.value)}
+                    />
+                  </div>
+
+                  <div className="w-full">
+                    <span className="text-md font-medium ml-1">
+                      Delivery Time
+                    </span>
+                    <div className="relative cursor-pointer">
+                      <ChevronDown className="size-6 absolute right-5 top-3 pointer-events-none" />
+                      <select
+                        className="px-3 py-2 w-full rounded-xl outline-none appearance-none border-2 border-[#d9d9d9] bg-white"
+                        value={standarddeliveryTime}
+                        onChange={(e) =>
+                          setstandardDeliveryTime(e.target.value)
+                        }
+                      >
+                        <option>Delivery</option>
+                        {deliveryOptions.map((dates) => (
+                          <option key={dates.value}>{dates.label}</option>
+                        ))}
+                      </select>
+                    </div>
+                  </div>
+
+                  <div className="w-full">
+                    <span className="text-md font-medium ml-1">Price</span>
+                    <input
+                      type="text"
+                      min={1}
+                      className="p-3 w-full border-2 border-[#d7d7d7] rounded-xl outline-none resize-none font-semibold text-md"
+                      value={standardprice}
+                      onChange={handlestandardPriceChange}
+                    />
+                  </div>
+                </div>
+
+                <div className="border border-black rounded-lg p-4 gap-4 flex flex-col">
+                  <h3 className="text-lg font-semibold mb-2 text-center">
+                    Premium
+                  </h3>
+
+                  <div className="w-full">
+                    <span className="text-md font-medium ml-1">
+                      Package Title
+                    </span>
+                    <textarea
+                      className="p-3 w-full border-2 border-[#d7d7d7] rounded-xl outline-none resize-none font-semibold text-md"
+                      value={premiumpriceTitle}
+                      onChange={(e) => setpremiumPriceTitle(e.target.value)}
+                    />
+                  </div>
+
+                  <div className="w-full">
+                    <span className="text-md font-medium ml-1">
+                      Package features
+                    </span>
+                    <textarea
+                      className="p-3 w-full border-2 border-[#d7d7d7] rounded-xl outline-none resize-none font-medium text-sm"
+                      value={premiumpriceFeatures}
+                      onChange={(e) => setpremiumPriceFeatures(e.target.value)}
+                    />
+                  </div>
+
+                  <div className="w-full">
+                    <span className="text-md font-medium ml-1">
+                      Delivery Time
+                    </span>
+                    <div className="relative cursor-pointer">
+                      <ChevronDown className="size-6 absolute right-5 top-3 pointer-events-none" />
+                      <select
+                        className="px-3 py-2 w-full rounded-xl outline-none appearance-none border-2 border-[#d9d9d9] bg-white"
+                        value={premiumdeliveryTime}
+                        onChange={(e) => setpremiumDeliveryTime(e.target.value)}
+                      >
+                        <option>Delivery</option>
+                        {deliveryOptions.map((dates) => (
+                          <option key={dates.value}>{dates.label}</option>
+                        ))}
+                      </select>
+                    </div>
+                  </div>
+
+                  <div className="w-full">
+                    <span className="text-md font-medium ml-1">Price</span>
+                    <input
+                      type="text"
+                      min={1}
+                      className="p-3 w-full border-2 border-[#d7d7d7] rounded-xl outline-none resize-none font-semibold text-md"
+                      value={premiumprice}
+                      onChange={handlepremiumPriceChange}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          )}
+          {currentStep === 2 && (
+            <div className="w-full p-4 sm:p-6 md:p-8 bg-white rounded-xl ">
+              <h1 className="mb-3 pl-1 font-semibold">
+                Showcase your gig with a compelling description—highlight your
+                skills, process, and what makes your service stand out
+              </h1>
+              <textarea
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                className={`p-3 w-full min-h-[300px] border-2   ${
+                  wordCount > 1000 ? "border-red-500" : "border-[#d7d7d7]"
+                } rounded-xl outline-none resize-none font-normal text-lg`}
+              />
+              <div className="w-full flex  justify-end">
+                <span
+                  className={`${
+                    wordCount > 1000 ? "text-red-500" : "text-black"
+                  }`}
+                >
+                  {wordCount}/1000 characters
+                </span>
+              </div>
+            </div>
+          )}
+          {currentStep === 3 && (
+            <div className="w-full flex items-center justify-center flex-col p-4 sm:p-6 md:p-8 bg-white rounded-xl ">
+              <h1 className="mb-3 pl-1 font-semibold">
+                Upload a relevant thumbnail that matches your gig's content
+              </h1>
+              <label
+                htmlFor="thumbnail"
+                className="max-h-[430px] min-h-[430px] min-w-[712px] max-w-[712px] overflow-hidden relative cursor-pointer"
+              >
+                <div className="  flex flex-col items-center justify-center border-1 border-dashed">
+                  <input
+                    type="file"
+                    accept="image/*"
+                    className="hidden"
+                    id="thumbnail"
+                    onChange={cacheImage}
+                    ref={fileuploadRef}
+                  />
+                  <XCircleIcon
+                    className=" absolute top-2 right-2 size-8 text-white"
+                    onClick={(e) => {
+                      e.preventDefault();
+                      e.stopPropagation();
+                      setPhoto("");
+                    }}
+                  />
+                  {photo ? (
+                    <img src={photo} className="w-full h-full" />
+                  ) : (
+                    <img
+                      src={uploadImage}
+                      className="max-h-[400px] min-h-[400px] min-w-[50px]"
+                    />
+                  )}
+
+                  <span
+                    className={`font-medium text-lg ${
+                      photo ? "hidden" : "block"
+                    }`}
+                  >
+                    upload image
+                  </span>
+                </div>
+              </label>
+            </div>
+          )}
         </div>
 
-        <div className="mt-8 flex flex-col sm:flex-row gap-3 sm:gap-6 items-center">
+        <div className="mt-8 flex flex-row sm:flex-row gap-3 sm:gap-6 items-center">
           <button
             onClick={() => setCurrentStep((prev) => Math.max(prev - 1, -1))}
             className={`px-5 py-2 rounded text-white ${
@@ -187,7 +507,7 @@ const PostGig = () => {
             onClick={() =>
               setCurrentStep((prev) => Math.min(prev + 1, steps.length - 1))
             }
-            className={`px-5 py-2 rounded text-white ${
+            className={`px-8 py-2 rounded text-white ${
               nextDisable
                 ? "bg-gray-400 cursor-not-allowed"
                 : "bg-lime-900 hover:bg-lime-800 cursor-pointer"
---
+++ frontend/src/utils/deliveryDates.js
@@ -0,0 +1,12 @@
+const deliveryOptions = [
+  { label: "1 Day Delivery", value: 1 },
+  { label: "2 Days Delivery", value: 2 },
+  { label: "3 Days Delivery", value: 3 },
+  { label: "4 Days Delivery", value: 4 },
+  { label: "5 Days Delivery", value: 5 },
+  { label: "7 Days Delivery", value: 7 },
+  { label: "10 Days Delivery", value: 10 },
+  { label: "15 Days Delivery", value: 15 },
+];
+
+export default deliveryOptions;
