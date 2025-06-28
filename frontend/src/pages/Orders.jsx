8ee341da43deb5cedbb78bb0fc310b49aa152d02
ELLA LAKSHMAN <laxmannani960@gmail.com>
2025-06-28 20:22:48 +0530

    chnages in the order

--- frontend/src/pages/Orders.jsx
+++ frontend/src/pages/Orders.jsx
@@ -6,6 +6,7 @@ import { useAuth } from "../hooks/useAuth";
 const Orders = () => {
   const { checkAuth } = useAuth();
   const [showError, setshowError] = useState(false);
+  const [ordersData, setordersData] = useState([]);
   const [error, setError] = useState(
     "something went wrong while fetching orders"
   );
@@ -28,6 +29,13 @@ const Orders = () => {
         errorText={error}
         errorStyles={`absoulte top-25`}
       />
+
+      {ordersData.length > 0 && (
+        <div>
+          <h1>order name</h1>
+        </div>
+      )}
+
       <div className="w-full flex  items-center justify-center flex-col">
         <img src={empty} className="w-[380px] h-[380px]" />
         <h1 className="text-xl font-semibold text-black">No orders yet!</h1>
