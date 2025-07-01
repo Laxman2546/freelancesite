8130a50c22518f621e8d6142b41b3e3ae59d3847
ELLA LAKSHMAN <laxmannani960@gmail.com>
2025-07-01 13:09:37 +0530

    started the gig details

--- frontend/src/components/PostedgigDetails.jsx
+++ frontend/src/components/PostedgigDetails.jsx
@@ -2,6 +2,8 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import Loader from "./Loader";
+import FreelancerNavbar from "./FreelancerNavbar";
+import { Cart, TimeSharp } from "react-ionicons";
 const PostedgigDetails = () => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
@@ -33,9 +35,34 @@ const PostedgigDetails = () => {
     getGigs();
   }, []);
   return (
-    <div>
+    <div className="w-full min-h-screen">
       {loading && <Loader />}
-      <h1>hello</h1>
+      <FreelancerNavbar />
+      <div className="w-full min-h-screen flex flex-col gap-5 bg-[#F4F2EE] p-2 md:p-8 ">
+        <div className="w-full h-full   flex flex-row  gap-3 items-center">
+          <div className="p-2 pl-4 pr-4 bg-[#C0E6FB] text-[#36ADF2] rounded-3xl text-center">
+            <span className="font-semibold text-sm md:text-md">Active</span>
+          </div>
+          <div className="p-2 pl-2 pr-2 sm:pl-4 sm:pr-4 bg-[#CDEBC1] overflow-hidden text-[#294018] rounded-3xl text-center text-nowrap">
+            <span className="font-semibold text-sm md:text-md">
+              {data.category}
+            </span>
+          </div>
+        </div>
+        <div className="mt-2 w-full md:max-w-2/3">
+          <h1 className=" text-2xl md:text-3xl font-semibold ">{data.title}</h1>
+        </div>
+        <div className="w-full flex flex-row">
+          <div className="w-full flex flex-row gap-2 items-center">
+            <Cart width={"18px"} height={"18px"} />
+            <span className="text-md  text-gray-600">250 orders in queue</span>
+          </div>
+          <div className="w-full flex flex-row gap-2 items-center">
+            <TimeSharp width={"18px"} height={"18px"} />
+            <span className="text-md  text-gray-600">Last updated</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
